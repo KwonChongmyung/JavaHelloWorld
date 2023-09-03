1e09392382cfef3f226afa853c99d4d60ceb08f0
chongmyung <mcwon3302@naver.com>
2023-09-03 16:01:25 +0900

    2st commit

---
+++ src/com/example/lab1/WordCRUD.java
@@ -0,0 +1,33 @@
+package com.example.lab1;
+import java.util.ArrayList;
+public class WordCRUD implements ICRUD{
+	ArrayList<Word> list;
+	
+	WordCRUD(){
+		list = new ArrayList<>();
+	}
+	@Override
+	public Object add() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public int update(Object obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Object obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void selectOne(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- src/com/example/lab1/main.java
+++ src/com/example/lab1/main.java
@@ -3,6 +3,6 @@ package com.example.lab1;
 public class main {
 	public static void main(String[] args)
 	{
-		System.out.println("hello world");
+		new WordManager().start();
 	}
 }
